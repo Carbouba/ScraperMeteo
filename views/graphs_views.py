a9d3a616f481f0d3ef695fe345409316cc99dfc5
Boubacar <saniboubacar1992@gmail.com>
2026-03-14 12:47:05 +0100

    api_calls.py_OK queries.py_OK graphs_started

---
+++ views/graphs_views.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from data.queries import get_temp_date
+
+data = get_temp_date
+temp = get_temp_date[0]
+date = get_temp_date[1]
+
+print(temp)
+print(date)
